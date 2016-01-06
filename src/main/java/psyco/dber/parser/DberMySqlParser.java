7750619ec07a44c0fb7c52a0513a23db29af1cf2
lipeng <dianxiangan@aliyun.com>
2016-01-06 19:41:52 +0800

    sdf

--- src/main/java/psyco/dber/parser/DberMySqlParser.java
+++ src/main/java/psyco/dber/parser/DberMySqlParser.java
@@ -1,42 +1,38 @@
 package psyco.dber.parser;
 
-import org.antlr.runtime.TokenRewriteStream;
-import org.antlr.runtime.tree.CommonTree;
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.RecognitionException;
-import psyco.dber.parser.gen.MySQLLexer;
-import psyco.dber.parser.gen.MySQLParser;
-
-import java.io.IOException;
+import org.antlr.v4.runtime.misc.TestRig;
+import org.junit.Test;
 
 /**
  * Created by lipeng on 16/1/6.
  */
 public class DberMySqlParser {
 //
-    public static MySQLWalker.beg_return parse(String sql) throws RecognitionException, IOException {
-
-        MySQLLexer pl = new MySQLLexer(new ANTLRInputStream(sql));
-        CommonTokenStream tokens = new CommonTokenStream(pl);
-        MySQLParser pa = new MySQLParser(tokens);
-        pa.expression();
-        CommonTree tree = (CommonTree) beg.getTree();
-
-        if (logger.isDebugEnabled()) {
-            logger.debug(tree.toStringTree());
-        }
-
-        CommonTreeNodeStream nodes = new CommonTreeNodeStream(tree);
-        nodes.setTokenStream(tokens);
-        MySQLWalker walker = new MySQLWalker(nodes);
-        walker.setGroupFunc(GroupFunctionRegister.reg);
-        walker.setFunctionMap(FunctionRegister.funcReg);
-        MySQLWalker.beg_return ret = walker.beg();
-
-        ret.obj.setPos2TableName(walker.pos2TableName);
-
-        return ret;
-
+//    public static MySQLWalker.beg_return parse(String sql) throws RecognitionException, IOException {
+//
+//        MySQLLexer pl = new MySQLLexer(new ANTLRInputStream(sql));
+//        CommonTokenStream tokens = new CommonTokenStream(pl);
+//        MySQLParser pa = new MySQLParser(tokens);
+//        CommonTree tree = (CommonTree) beg.getTree();
+//
+//        if (logger.isDebugEnabled()) {
+//            logger.debug(tree.toStringTree());
+//        }
+//
+//        CommonTreeNodeStream nodes = new CommonTreeNodeStream(tree);
+//        nodes.setTokenStream(tokens);
+//        MySQLWalker walker = new MySQLWalker(nodes);
+//        walker.setGroupFunc(GroupFunctionRegister.reg);
+//        walker.setFunctionMap(FunctionRegister.funcReg);
+//        MySQLWalker.beg_return ret = walker.beg();
+//
+//        ret.obj.setPos2TableName(walker.pos2TableName);
+//
+//        return ret;
+//
+//    }
+    @Test
+    public void sdf() throws Exception {
+        TestRig.main("Java8 -gui".split("\\s+"));
     }
 }
